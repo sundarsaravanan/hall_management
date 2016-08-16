f7ad65023164e8ff37198c68a650ab3a26a18ba6
sundarsaravanan <sundarsaravanan96@gmail.com>
2016-08-16 22:47:24 +0530

    updated

--- confirmation.php
+++ confirmation.php
@@ -8,8 +8,10 @@ else{
 	 $uid=$_SESSION['id1'];
 	$uspass = $_SESSION['pass2'];
     $usname = $_SESSION['username2'];
+    $reference=$_SESSION['username1'];
+        $fullname=$_SESSION['fullname'];
 	include_once("dbconnect.php");
-	$query = "INSERT INTO staff"."(id,name,password)"."VALUES (NULL,'$usname','$uspass')";
+	$query = "INSERT INTO staff"."(id,full_name,username,password,reference)"."VALUES (NULL,'$fullname','$usname','$uspass','$reference')";
 	$result = mysqli_query($dbCon, $query)
 	or die('Error querying database.');
 	mysqli_close($dbCon);
